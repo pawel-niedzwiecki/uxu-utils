86d4addcd46716fe5028445b4f3887b6433805d7
Paweł Niedźwiecki <hello@uxu.pl>
2022-12-17 20:26:25 +0100

    feat: add new config in theme for border

--- libs/design-system/src/lib/theme/theme.ts
+++ libs/design-system/src/lib/theme/theme.ts
@@ -1,30 +1,32 @@
-import { breakpoints } from './breakpoints';
-import { colors } from './colors';
-import { focuses } from './focuses';
-import { fontSizes } from './fontSizes';
-import { fontWeights } from './fontWeights';
-import { lineHeights } from './lineHeights';
-import { motion } from './motion';
-import { radii } from './radii';
-import { shadows } from './shadows';
-import { space } from './space';
-import { grid } from './grid';
-import { flex } from './flex';
-import { content } from './content';
+import {breakpoints} from './breakpoints';
+import {colors} from './colors';
+import {focuses} from './focuses';
+import {fontSizes} from './fontSizes';
+import {fontWeights} from './fontWeights';
+import {lineHeights} from './lineHeights';
+import {motion} from './motion';
+import {radii} from './radii';
+import {shadows} from './shadows';
+import {space} from './space';
+import {grid} from './grid';
+import {flex} from './flex';
+import {content} from './content';
+import {borders} from "./borders";
 
-export { breakpoints } from './breakpoints';
-export { colors } from './colors';
-export { focuses } from './focuses';
-export { fontSizes } from './fontSizes';
-export { fontWeights } from './fontWeights';
-export { lineHeights } from './lineHeights';
-export { motion } from './motion';
-export { radii } from './radii';
-export { shadows } from './shadows';
-export { space } from './space';
-export { grid } from './grid';
-export { flex } from './flex';
-export { content } from './content';
+export {breakpoints} from './breakpoints';
+export {colors} from './colors';
+export {focuses} from './focuses';
+export {fontSizes} from './fontSizes';
+export {fontWeights} from './fontWeights';
+export {lineHeights} from './lineHeights';
+export {motion} from './motion';
+export {radii} from './radii';
+export {shadows} from './shadows';
+export {space} from './space';
+export {grid} from './grid';
+export {flex} from './flex';
+export {content} from './content';
+export {borders} from './borders';
 
 export type Theme = typeof theme;
 
@@ -42,4 +44,5 @@ export const theme = {
   grid,
   flex,
   content,
+  borders
 } as const;
