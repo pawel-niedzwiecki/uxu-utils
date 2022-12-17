931858f1e432abedf701bcd3ae285b3858d4d12f
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2022-12-17 20:00:00 +0100

    fix: component grid (#57)

--- libs/design-system/src/lib/components/atoms/flex/col/commponent.flex.col.tsx
+++ libs/design-system/src/lib/components/atoms/flex/col/commponent.flex.col.tsx
@@ -1,5 +1,5 @@
-import {ColStyle} from './commponent.flex.col.style';
+import {Wrapper} from './commponent.flex.col.style';
 import type {Props} from './commponent.flex.col.props'
 
-export const Col: Props = ({children, ...args}, props) => <ColStyle {...props} {...args} cols={args}>{children}</ColStyle>
+export const Col: Props = ({children, ...args}, props) => <Wrapper {...props} {...args} cols={args}>{children}</Wrapper>
 
