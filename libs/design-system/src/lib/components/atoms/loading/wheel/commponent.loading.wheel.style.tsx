d55c9ca91fe0569de56fa5e04188c9e358251548
Paweł Niedźwiecki <hello@uxu.pl>
2022-12-19 23:43:12 +0100

    fix: fix style for loading wheel

--- libs/design-system/src/lib/components/atoms/loading/wheel/commponent.loading.wheel.style.tsx
+++ libs/design-system/src/lib/components/atoms/loading/wheel/commponent.loading.wheel.style.tsx
@@ -1,95 +1,42 @@
 import styled, {css} from 'styled-components';
 import {SpecialProps} from './commponent.loading.wheel.props'
 
+
 export const Wrapper = styled.div<SpecialProps>`
+  ${({theme: {space}, size}) => css`
+    width: ${size}rem;
+    height: ${size}rem;
+  `}
+`
+
+export const Wheel = styled.div<SpecialProps & { el: undefined[] }>`
   ${({theme: {space, radii}, size}) => css`
-    display: flex;
+
+    top: 50%;
+    left: 50%;
     width: ${size}rem;
     height: ${size}rem;
     position: relative;
 
     span {
+      top: -12%;
+      left: -18%;
       width: 24%;
       height: 8%;
       display: block;
+      position: absolute;
       margin: ${space.small};
       border-radius: ${radii.default};
       background-color: var(--uxu-color-primary-accent6);
-      animation-name: loading-dots_blink;
+      animation-name: loading-wheel-blink;
       animation-duration: 1.4s;
       animation-fill-mode: both;
       animation-iteration-count: infinite;
 
-      height: 8%;
-      left: -10%;
-      position: absolute;
-      top: -3.9%;
-      width: 24%;
-
-      &:nth-child(1){
-        transform: rotate(0deg) translate(146%);
-        animation-delay: -1.2s;
-      }
-
-      &:nth-child(2){
-        transform: rotate(30deg) translate(146%);
-        animation-delay: -1.1s;
-      }
-
-      &:nth-child(3){
-        transform: rotate(60deg) translate(146%);
-        animation-delay: -1s;
-      }
-
-      &:nth-child(4){
-        transform: rotate(90deg) translate(146%);
-        animation-delay: -0.9s;
-      }
-
-      &:nth-child(5){
-        transform: rotate(120deg) translate(146%);
-        animation-delay: -0.8s;
-      }
-
-      &:nth-child(6){
-        transform: rotate(150deg) translate(146%);
-        animation-delay: -0.7s;
-      }
-
-
-      &:nth-child(7){
-        transform: rotate(180deg) translate(146%);
-        animation-delay: -0.6s;
-      }
-
-      &:nth-child(8){
-        transform: rotate(220deg) translate(146%);
-        animation-delay: -0.5s;
-      }
-
-      &:nth-child(9){
-        transform: rotate(250deg) translate(146%);
-        animation-delay: -0.4s;
-      }
-
-      &:nth-child(10){
-        transform: rotate(280deg) translate(146%);
-        animation-delay: -0.3s;
-      }
-
-      &:nth-child(11){
-        transform: rotate(330deg) translate(146%);
-        animation-delay: -0.2s;
-      }
-
-      &:nth-child(12){
-        transform: rotate(360deg) translate(146%);
-        animation-delay: -0.1s;
-      }
-
+      ${children};
     }
 
-    @keyframes loading-dots_blink {
+    @keyframes loading-wheel-blink {
       0% {
         opacity: 1
       }
@@ -97,5 +44,25 @@ export const Wrapper = styled.div<SpecialProps>`
         opacity: 0.15
       }
     }
+
   `}
+`;
+
+
+const children = css<SpecialProps & { el: undefined[] }>`
+  ${({el}) => {
+  let style = {}
+  const count = el.length;
+
+  for (let i = 0; i < count; i++) {
+    Object.assign(style, {
+      [`&:nth-child(${i + 1})`]: {
+        animationDelay: `${0 - (i / 10)}s`,
+        transform: `rotate(${360 / count * i}deg) translate(146%)`
+      }
+    })
+  }
+
+  return style
+}};
 `
--- libs/design-system/src/lib/components/atoms/loading/wheel/components.loading.wheel.tsx
+++ libs/design-system/src/lib/components/atoms/loading/wheel/components.loading.wheel.tsx
@@ -1,8 +1,12 @@
-import {Wrapper} from "./commponent.loading.wheel.style";
+import {Wrapper, Wheel} from "./commponent.loading.wheel.style";
 import {Props} from './commponent.loading.wheel.props';
 
+const el = new Array(12).fill(undefined)
+
 export const LoadingWheel: Props = ({...args}, props) => (
-  <Wrapper {...args} {...props} >
-    {new Array(12).fill(undefined).map((item, i) => <span></span>)}
+  <Wrapper {...args} {...props}>
+    <Wheel size={args.size} el={el}>
+      {el.map((item, i) => <span></span>)}
+    </Wheel>
   </Wrapper>
 )
