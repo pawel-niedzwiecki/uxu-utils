ad96cf90de9ad46142c70c445549d59411cdca05
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2022-12-20 19:48:05 +0100

    feat: add section for storybook (#73)

--- libs/design-system/src/lib/components/molecules/input/searchInput/components.input.searchInput.tsx
+++ libs/design-system/src/lib/components/molecules/input/searchInput/components.input.searchInput.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import type {Props} from './components.input.searchInput.props'
 import {ComponentBox} from './components.input.searchInput.style'
 
-export const SearchInput: Props = ({children, ...args}, props) => <ComponentBox {...props} {...args}>{children}</ComponentBox>
+export const SearchInput: Props = ({children, ...args}, props) => {
+  return (
+    <ComponentBox {...props} {...args}>{children}</ComponentBox>
+  )
+}
 
---
+++ libs/design-system/src/lib/components/templates/section/storybook/index.ts
@@ -0,0 +1,2 @@
+
+export * from './components.section.storybook';
