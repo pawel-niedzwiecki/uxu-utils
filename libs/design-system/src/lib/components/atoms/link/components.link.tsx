0d91229d9451e68659531800f2a4361f4fac4552
Paweł Niedźwiecki <hello@uxu.pl>
2022-12-26 00:35:25 +0100

    fix: delete console.log()

--- libs/design-system/src/lib/components/atoms/link/components.link.tsx
+++ libs/design-system/src/lib/components/atoms/link/components.link.tsx
@@ -6,10 +6,7 @@ import {LoadingWheel} from "components/atoms/loading";
 
 
 export const Link: Props = ({children, ...args}, props) => {
-  console.log(args?.href)
-
   const url = regexURL.test(args?.href);
-  console.log(url)
   const content = args.variant === 'loading' ? <><LoadingWheel size={2} style={{marginRight: "0.75rem"}}/> Loading</> : <>{args?.prefix}{children}{args?.suffix}</>;
   if (url) return <Url {...args} {...props} target="_blank" rel="noopener noreferrer">{content}</Url>;
   else return <Slug {...args} {...props}>{content}</Slug>;
