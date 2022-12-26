2073891fbe13218a2e985a7bfcfb53f5eb99626f
Paweł Niedźwiecki <hello@uxu.pl>
2022-12-26 21:34:30 +0100

    feat: add switch

--- libs/design-system/src/lib/components/atoms/grid/commponent.grid.style.tsx
+++ libs/design-system/src/lib/components/atoms/grid/commponent.grid.style.tsx
@@ -1,10 +1,16 @@
 import styled, {css} from 'styled-components';
-import {listensPropsGrid, listensPropsColor, listensPropsBorder, listensPropsShadow, listensPropsSize} from '@uxu/props-styled-system';
+import {
+  listensPropsGrid,
+  listensPropsColor,
+  listensPropsBorder,
+  listensPropsShadow,
+  listensPropsSize
+} from '@uxu/props-styled-system';
 
 import {SpecialProps} from "./commponent.grid.props";
 
 export const Wrapper = styled.div<SpecialProps>`
-  ${({theme: {space, breakpoints, flex}, container}) => css`
+  ${({container}) => css`
     ${listensPropsGrid}
     ${listensPropsShadow};
     ${listensPropsColor};
