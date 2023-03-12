71597bbf4fb548b7d4284a41fb51f68929a5ecd0
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-12 15:26:30 +0100

    Development lib design system components (#140)
    * fix: fix name file with props for col & box
    
    * fix: fix stories for component box (#25)
    
    * feat: add new stories (#28)
    
    * fix: fix list variabels color & add stories for all colors (#29)
    
    * feat: add icons (#41)
    
    * fix: merge fix
    
    * feat: add config for grid (#13)
    
    * ferat: add config for position (#14)
    
    * feat: add config for topografy (#15)
    
    * feat: add config for shadow (#16)
    
    * fix: fix merge
    
    * fix: fix merge
    
    * Feat/dev lib design system components logo (#51)
    
    * feat: add hooks for mode
    
    * feat: add assets
    
    * feat: add new component Logo with stories
    
    * fix: component grid (#57)
    
    * feat: add new config in theme for border (#62)
    
    * feat: add story for grid and fix other stories (#65)
    
    * fix: fix component txt (#68)
    
    * Feat/dev lib design system components loading (#69)
    
    * feat: add loading dots
    
    * fix: update settings for storybook
    
    * fix: delete direction from settings storybook
    
    * feat: add style for wheel
    
    * fix: fix name in storybook
    
    * fix: fix style for loading wheel
    
    * fix: fix export all (#72)
    
    * feat: add section for storybook (#73)
    
    * fix: fix stories for box (#74)
    
    * fix: fix stories for flex (#75)
    
    * feat: update nx (#76)
    
    * Feat/dev lib design system components button (#79)
    
    * feat: add vairabels in global style css
    
    * feat: add color for button
    
    * feat: add style for all type buttons
    
    * Feat/dev lib design system components button stories (#80)
    
    * feat: add story for button shape
    
    * feat: add stories for button prefix and suffix
    
    * feat: add storys for variant
    
    * feat: add storys to button with Aligns
    
    * Feat/dev lib design system components input (#81)
    
    * feat: add style for input search
    
    * feat: add label
    
    * feat: add all style for input search
    
    * feat: add logix for sugestion
    
    * feat: add link
    
    * Feat/dev lib design system components link (#82)
    
    * feat: add link with stories
    
    * fix: delete console.log()
    
    * feat: update sotires for link & search
    
    * feat: add switch (#87)
    
    * Feat/dev lib design system components tabs (#88)
    
    * fix: fix path inports
    
    * fix: update path
    
    * feat: add components tabs with stories
    
    * fix: fix all stories for button (#91)
    
    * fix: delete libs @uxu/utils & @uxu/types & @uxu/prop-system-desing & add linter (#92)
    
    * Feat/dev lib design system components husky (#95)
    
    * feat: add husky & config for husky
    
    * test
    
    * test
    
    * add file config
    
    * add husky config
    
    * Feat/development lib design system components git actions (#100)
    
    * feat: add github actions demo
    
    * fix: fix commit
    
    * feat: add test
    
    * feat: add stage
    
    * fix: fix action
    
    * fix: dix action
    
    * fix: fix action
    
    * fix: update workflow
    
    * fix: add compile
    
    * fix: update adction install
    
    * fix: fix install
    
    * fix: fix action
    
    * fix: fix action install
    
    * fix: fix action install
    
    * fix: fix action install
    
    * fix: fix action install
    
    * fix: update action
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix action
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * Feat/development lib design system components git actions (#101)
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: update compile
    
    * fix: update
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix path (#108)
    
    * feat: add export (#110)
    
    * Fix/development lib design system components storybook (#111)
    
    * feat: add export
    
    * fix: fix storybook
    
    * feat: add header for desktop (#118)
    
    * feat: add service nextjs components (#120)
    
    * Feat/development lib design system components header (#121)
    
    * feat: add header for desktop
    
    * feat: add style switch && add header
    
    * fix: header && provider (wrapper) (#122)
    
    * fix: hook (#124)
    
    * Fix/hooks (#129)
    
    * fix: hook
    
    * fix: fix switch theme
    
    * fix: fix
    
    * fix: add import for utils (#134)
    
    * fix: fix typescript (#139)

--- libs/design-system/src/lib/components/molecules/input/input/component.input.tsx
+++ libs/design-system/src/lib/components/molecules/input/input/component.input.tsx
@@ -1,3 +1,7 @@
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { forwardRef } from 'react';
 import type { Props } from './component.input.types';
 import { InputComponent, LabelComponent } from './component.input.style';
--- libs/design-system/src/lib/utils/function/index.ts
+++ libs/design-system/src/lib/utils/function/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export * from './throttle'
 export * from './checkIsDOM'
-=======
-export * from './throttle';
-export * from './checkIsDOM';
->>>>>>> development
