7cc048a91df4d914d89a73ff9896068c922f47b1
Paweł Niedźwiecki <hello@uxu.pl>
2023-03-12 15:50:28 +0100

    fix: fix ts

--- libs/design-system/src/lib/components/molecules/input/input/component.input.tsx
+++ libs/design-system/src/lib/components/molecules/input/input/component.input.tsx
@@ -1,14 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { forwardRef } from 'react';
 import type { Props } from './component.input.types';
 import { InputComponent, LabelComponent } from './component.input.style';
 
-export const Input = (props: any) => {
-  // @ts-ignore
+export const Input = forwardRef<Props>((props, ref) => {
   const { className, children, style, ...otherProps } = props;
   return (
     <LabelComponent className={className} style={style}>
-      <InputComponent {...otherProps} />
+      <InputComponent {...otherProps} ref={ref} />
       {children}
     </LabelComponent>
   );
-};
-
+});
--- libs/design-system/src/lib/components/organisms/form/search/component.search.tsx
+++ libs/design-system/src/lib/components/organisms/form/search/component.search.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import React, { FormEvent, useEffect, useState } from 'react';
 import { Search } from 'react-feather';
 import { useForm } from 'react-hook-form';
