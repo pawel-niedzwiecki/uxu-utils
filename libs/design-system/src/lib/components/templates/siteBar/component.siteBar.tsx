1abe21abdc1a53f6e0042c8762cf6dd90327f55e
Paweł Niedźwiecki <hello@uxu.pl>
2023-03-25 23:51:47 +0100

    fix: fix siteBar

--- libs/design-system/src/lib/components/templates/siteBar/component.siteBar.tsx
+++ libs/design-system/src/lib/components/templates/siteBar/component.siteBar.tsx
@@ -8,11 +8,11 @@ import { SiteBarCover } from './component.siteBar.fragment.cover';
 import { SiteBarFilter } from './component.siteBar.fragment.filter';
 import { SiteSocialMedia } from './component.siteBar.fragment.socialMedia';
 
-export const SiteBar: Props = ({ data: { cover, filter, socialMedia } }) => {
+export const SiteBar: Props = ({ cover, filter, socialMedia }) => {
   const { hiddeMenu } = useMenu();
   const [open, setOpen] = useState(false);
 
-  return (
+  if (cover || filter || socialMedia) return (
     <Wrapper open={open} hiddeMenu={hiddeMenu}>
       <Box top={hiddeMenu ? 15 : 115}>
         <Button
@@ -28,4 +28,5 @@ export const SiteBar: Props = ({ data: { cover, filter, socialMedia } }) => {
       </Box>
     </Wrapper>
   );
+  else return null;
 };
