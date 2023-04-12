ed3c860f58fe1db78d9155bb27bc050111f7571c
Paweł Niedźwiecki <hello@uxu.pl>
2023-04-12 02:10:29 +0200

    fix: type

--- libs/design-system/src/lib/components/organisms/article/full/components.article.full.types.tsx
+++ libs/design-system/src/lib/components/organisms/article/full/components.article.full.types.tsx
@@ -1,28 +1,26 @@
 import { FC } from 'react';
 import type { FunctionComponentDiv } from './../../../../utils';
 
-
 export type ArticleDataType = {
   content: {
-    lead: string
-    type: string
-    title: string
-    createdAt: Date
-    cover: { src?: string, alt?: string }
+    lead: string;
+    type: string;
+    title: string;
+    createdAt: Date;
+    cover: { src?: string; alt?: string } | null;
     author: {
-      name: string
-      avatar?: { src?: string, alt?: string }
-    }
-    tags?: { title: string, slug: string }[]
-    stats?: { ratings: number, comments: number, views: number }
-    contentparts?: { __typename: string, content?: string, src?: string, alt?: string }[]
-  }
-}
+      name: string;
+      avatar?: { src?: string | null; alt?: string | null };
+    };
+    tags?: { title: string; slug: string }[];
+    stats?: { ratings: number; comments: number; views: number };
+    contentparts?: { __typename: string; content?: string; src?: string | null; alt?: string | null }[];
+  };
+};
 
 export type SpecialProps = {
-  isLoading: boolean
-  data: ArticleDataType
-}
-
+  isLoading: boolean;
+  data: ArticleDataType;
+};
 
 export type Props = FC<FunctionComponentDiv & SpecialProps>;
--- libs/design-system/src/lib/components/organisms/article/short/components.article.short.types.tsx
+++ libs/design-system/src/lib/components/organisms/article/short/components.article.short.types.tsx
@@ -1,27 +1,27 @@
 import { FC } from 'react';
 import type { FunctionComponentDiv } from './../../../../utils';
 
-
 export type ArticleShortDataType = {
   content: {
-    id: string
-    cover?: { src?: string, alt?: string }
-    author: {
-      name: string
-      avatar?: { src?: string, alt?: string }
-    }
-    tags?: { title: string, slug: string }[]
-    stats?: { ratings: number, comments: number, views: number }
-    title: string
-    slug: string
-    createdAt: Date
-  }
-}
+    id: string;
+    cover?: { src?: string; alt?: string } | undefined;
+    author:
+      | {
+          name: string;
+          avatar?: { src?: string | undefined; alt?: string | undefined };
+        }
+      | undefined;
+    tags?: { title: string; slug: string }[];
+    stats?: { ratings: number; comments: number; views: number };
+    title: string;
+    slug: string;
+    createdAt: Date;
+  };
+};
 
 export type SpecialProps = {
-  isLoading: boolean
-  data: ArticleShortDataType
-}
-
+  isLoading: boolean;
+  data: ArticleShortDataType;
+};
 
 export type Props = FC<FunctionComponentDiv & SpecialProps>;
