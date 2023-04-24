4c07c81337633a0ef289a45bdf5e08fd55f8795f
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-04-24 21:10:05 +0200

    Feat/add footer (#196)
    * feat: add footer
    
    * feat: add style for footer

--- libs/design-system/src/lib/components/templates/footer/component.footer.tsx
+++ libs/design-system/src/lib/components/templates/footer/component.footer.tsx
@@ -1,5 +1,5 @@
-import { Wrapper } from './component.footer.styles';
-import { Box, Container, Grid, Logo } from '../../atoms';
+import { Header, Wrapper } from './component.footer.styles';
+import { Container, Grid, Link, Logo } from '../../atoms';
 import React from 'react';
 import { contents } from '../../../theme';
 
@@ -8,9 +8,38 @@ export function Footer() {
     <Wrapper>
       <Container>
         <Grid gridTemplateColumns={{ xs: '1fr', m: '250px 1fr 1fr 1fr', l: '250px 1fr 1fr 1fr 1fr ' }} style={{ maxWidth: contents.maxWidth, margin: '0 auto' }} container>
-          <Box width="25rem">
+          <Grid gridRow="1/30" className="brand">
             <Logo type="wTrasie" height={{ x: '2rem', s: '2.5rem' }} fill="primary.foreground" />
-          </Box>
+            <p>© 2023 UXU </p>
+          </Grid>
+
+          {new Array(5).fill(undefined).map(() => (
+            <Grid>
+              <Header>Miasta</Header>
+              <ul>
+                <li>
+                  <Link href="/" title="uxu">
+                    Warszawa
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" title="uxu">
+                    Poznań
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" title="uxu">
+                    Łódź
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" title="uxu">
+                    Gorzów Wielkopolski
+                  </Link>
+                </li>
+              </ul>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </Wrapper>
