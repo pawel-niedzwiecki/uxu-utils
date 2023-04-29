3064c4e3a28e42cdb23fcdbe803480bc6139e524
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-04-29 21:08:57 +0200

    Feat/add footer (#197)
    * feat: add footer
    
    * feat: add style for footer
    
    * feat: add data footer

--- libs/design-system/src/lib/components/templates/footer/component.footer.styles.tsx
+++ libs/design-system/src/lib/components/templates/footer/component.footer.styles.tsx
@@ -1,9 +1,8 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.footer`
-  ${({ theme: { spaces } }) => css`
+  ${({ theme: { spaces, breakpoints } }) => css`
     width: 100%;
-    min-height: 25rem;
     padding: ${spaces.large} 0;
     border-top: var(--uxu-border-default);
     background-color: var(--uxu-color-primary-accent1);
@@ -14,21 +13,44 @@ export const Wrapper = styled.footer`
       justify-content: space-between;
     }
 
+    p {
+      font-weight: bold;
+
+      a {
+        padding-left: ${spaces.small};
+      }
+    }
+
     ul {
       list-style: none;
       padding-bottom: ${spaces.large};
 
       li {
+        text-align: center;
         padding: ${spaces.small} 0;
       }
     }
+
+    @media all and (min-width: ${breakpoints.m}px) {
+      ul {
+        li {
+          text-align: left;
+        }
+      }
+    }
   `}
+}
 `;
 
 export const Header = styled.span`
-  ${({ theme: { spaces } }) => css`
+  ${({ theme: { spaces, breakpoints } }) => css`
     display: block;
     font-weight: bold;
+    text-align: center;
     padding-bottom: ${spaces.default};
+
+    @media all and (min-width: ${breakpoints.m}px) {
+      text-align: left;
+    }
   `}
 `;
