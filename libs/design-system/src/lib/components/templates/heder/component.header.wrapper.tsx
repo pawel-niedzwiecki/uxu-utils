f02334fa8ebcdee9fd345ee84ba08bc7924e5025
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-05-09 23:41:14 +0200

    refactor: header && alert (#225)

--- libs/design-system/src/lib/components/templates/heder/component.header.wrapper.tsx
+++ libs/design-system/src/lib/components/templates/heder/component.header.wrapper.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useBreakpoints } from './../../../hooks';
-import { ComponentType } from './component.header.types';
+import {  ComponentType } from './component.header.types';
 import { HeaderFull, HeaderShort } from './components';
 
-export const Header: ComponentType = ({ tabs, callBack, res, alert, ...args }, props) => {
+export const Header: ComponentType = ({ tabs, callBack, res, ComponentAaboveHeader, ...args }, props) => {
   const { isTabletOrMobile } = useBreakpoints();
 
   return (
     <>
+      <HeaderFull isMobile={isTabletOrMobile} tabs={tabs} callBack={callBack} res={res} ComponentAaboveHeader={ComponentAaboveHeader} />
       {!isTabletOrMobile && <HeaderShort isMobile={isTabletOrMobile} tabs={tabs} callBack={callBack} res={res} />}
-      <HeaderFull isMobile={isTabletOrMobile} tabs={tabs} alert={alert} callBack={callBack} res={res} />
     </>
   );
 };
