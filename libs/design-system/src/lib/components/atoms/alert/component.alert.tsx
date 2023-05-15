f0fce2bde13945199939c76a768e3a8cbc991208
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-05-15 14:20:51 +0200

    feat: add icon (#236)

--- libs/design-system/src/lib/components/atoms/alert/component.alert.tsx
+++ libs/design-system/src/lib/components/atoms/alert/component.alert.tsx
@@ -1,13 +1,14 @@
-import { Wrapper } from './component.alert.style';
 import { Link } from './..';
+import {Phone} from 'react-feather'
+import { Wrapper } from './component.alert.style';
 import { ComponentAlertType } from './component.alert.type';
 
 export const Alert: ComponentAlertType = ( {tel, title, ...props} ) => {
   return (
     <Wrapper {...props}>
       {tel && (
-        <Link href={`tel:${tel.replace ( /\s+/g, '' )}`} title={tel}>
-          {tel}
+        <Link href={`tel:${tel.replace ( /\s+/g, '' )}`} title={tel} style={{display: 'flex', justifyContent: 'center'}}>
+          <Phone style={{marginRight: "1.5rem"}}/>{tel}
         </Link>
       )}
       <span>{title}</span>
