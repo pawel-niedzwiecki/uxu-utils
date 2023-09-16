d14c7369ce7cf9c5ab89783473193796c6e4bd0a
Paweł Niedźwiecki <hello@uxu.pl>
2023-09-17 00:48:03 +0200

    refactor: feedback

--- libs/design-system/src/lib/components/atoms/feedback/feedback.tsx
+++ libs/design-system/src/lib/components/atoms/feedback/feedback.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Modal, Note, LoadingWheel } from '../../atoms';
 import { Input, Textarea } from '../../molecules/inputs';
 import styles from './feedback.module.scss';
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { FeedbackRatings } from './components/feedbackRatings';
 import { FormInputs } from './types';
 import { MESSAGE_SUCCESS, MESSAGE_FAILURE, FORM_FIELD } from './consts';
@@ -13,9 +13,10 @@ interface FeedbackProps {
   onFeedbackSubmit: (data: FormInputs) => Promise<{ success: boolean }>;
   activeUserEmail?: string;
   lastUserRating?: "smile" | "meh" | "frown";
+  modalAlignment?: 'left' | 'center' | 'right';
 }
 
-export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonText, lastUserRating }: FeedbackProps) {
+export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonText, lastUserRating, modalAlignment }: FeedbackProps) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
   const [activeFeedbackRating, setActiveFeedbackRating] = useState<string | null>(lastUserRating || null);
@@ -24,47 +25,34 @@ export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonT
   const { register, handleSubmit, formState: { errors }, watch, setValue } = useForm<FormInputs>();
   const feedbackRating = watch("feedbackRating");
 
-  const toggleModal = useCallback(() => {
-    setModalOpen(prev => !prev);
-  }, []);
+  const toggleModal = () => setModalOpen(prev => !prev);
 
-  const handleFormSubmit: SubmitHandler<FormInputs> = useCallback(async (data) => {
+  const handleFormSubmit = async (data: FormInputs) => {
     setIsSubmitting(true);
     try {
       const response = await onFeedbackSubmit(data);
-      setSubmitStatus({
-        success: response.success,
-        message: response.success ? MESSAGE_SUCCESS : MESSAGE_FAILURE,
-      });
-      setTimeout(() => {
-        setModalOpen(false);
-      }, 5000);
+      setSubmitStatus({ success: response.success, message: response.success ? MESSAGE_SUCCESS : MESSAGE_FAILURE });
+      setTimeout(() => setModalOpen(false), 5000);
     } catch {
-      setSubmitStatus({
-        success: false,
-        message: MESSAGE_FAILURE,
-      });
+      setSubmitStatus({ success: false, message: MESSAGE_FAILURE });
     } finally {
       setIsSubmitting(false);
     }
-  }, [onFeedbackSubmit]);
+  };
 
-  const handleFeedbackRatingChange = useCallback(async (value: string) => {
+  const handleFeedbackRatingChange = async (value: string) => {
     setActiveFeedbackRating(value);
-
     if (!switchModalButtonText) {
-      handleFormSubmit({email: activeUserEmail, feedbackRating: value, message: undefined})
+      handleFormSubmit({ email: activeUserEmail, feedbackRating: value, message: undefined });
     }
-  }, [handleSubmit, handleFormSubmit, switchModalButtonText, setValue, activeUserEmail]);
+  };
 
-
-
-  const handleFeedbackOutsideClick = useCallback((event: MouseEvent) => {
+  const handleFeedbackOutsideClick = (event: MouseEvent) => {
     if (modalWrapperRef.current && !modalWrapperRef.current.contains(event.target as Node) && isModalOpen) {
       setModalOpen(false);
       setSubmitStatus({});
     }
-  }, [isModalOpen]);
+  };
 
   useEffect(() => {
     if (activeFeedbackRating !== null) {
@@ -77,7 +65,7 @@ export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonT
     return () => document.removeEventListener("click", handleFeedbackOutsideClick);
   }, [handleFeedbackOutsideClick]);
 
-  const renderModalContent = useCallback(() => {
+  const renderModalContent = () => {
     if (isSubmitting) {
       return <Note type="warning" action={<LoadingWheel size={3}/>}>Zapisuję...</Note>;
     }
@@ -85,27 +73,10 @@ export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonT
     if (submitStatus.success === undefined) {
       return (
         <form onSubmit={handleSubmit(handleFormSubmit)} className={styles.formFeedback}>
-          {!activeUserEmail && (
-            <Input
-              placeholder="Adres email"
-              errorMessage={errors.email?.message}
-              {...register('email', FORM_FIELD.email)}
-              type="email"
-            />
-          )}
-          {switchModalButtonText && (
-            <Textarea
-              placeholder="Opisz swoje przemyślenia"
-              errorMessage={errors.message?.message}
-              {...register('message', FORM_FIELD.message)}
-            />
-          )}
+          {!activeUserEmail && <Input placeholder="Adres email" errorMessage={errors.email?.message} {...register('email', FORM_FIELD.email)} type="email" />}
+          {switchModalButtonText && <Textarea placeholder="Opisz swoje przemyślenia" errorMessage={errors.message?.message} {...register('message', switchModalButtonText ? { ...FORM_FIELD.message, required: 'To pole jest wymagane.' } : FORM_FIELD.message)} />}
           <div className={styles.footerModalFeedback}>
-            <FeedbackRatings
-              feedbackRating={feedbackRating}
-              handleFeedbackRatingChange={handleFeedbackRatingChange}
-              register={register}
-            />
+            <FeedbackRatings feedbackRating={feedbackRating} handleFeedbackRatingChange={handleFeedbackRatingChange} register={register} />
             {switchModalButtonText && <button className={classNames("btn", styles.btnSubmitFeedback)} type="submit">Wyślij</button>}
             {!!errors.feedbackRating?.message && <Note type="warning" className={styles.wrapperRatingNoteFeedback} fill>{errors.feedbackRating?.message}</Note>}
           </div>
@@ -115,26 +86,14 @@ export function Feedback({ onFeedbackSubmit, activeUserEmail, switchModalButtonT
 
     const noteType = submitStatus.success ? "success" : "error";
     return <Note type={noteType}>{submitStatus.message}</Note>;
-  }, [isSubmitting, submitStatus.success, handleSubmit, handleFormSubmit, register, errors, feedbackRating, handleFeedbackRatingChange, FORM_FIELD, activeUserEmail, switchModalButtonText]);
+  };
 
   return (
     <div className={styles.wrapperFeedback} ref={modalWrapperRef}>
-      <div className={styles.wrapperModalFeedback}>
-        <Modal open={isModalOpen} className={styles.modalFeedback}>
-          {renderModalContent()}
-        </Modal>
+      <div className={classNames(styles.wrapperModalFeedback, { [styles[`${modalAlignment}`]]: modalAlignment })}>
+        <Modal open={isModalOpen} className={styles.modalFeedback}>{renderModalContent()}</Modal>
       </div>
-      {switchModalButtonText ? (
-        <button className="btn" onClick={toggleModal}>
-          {switchModalButtonText}
-        </button>
-      ) : (
-        <FeedbackRatings
-          feedbackRating={feedbackRating}
-          handleFeedbackRatingChange={handleFeedbackRatingChange}
-          register={register}
-        />
-      )}
+      {switchModalButtonText ? <button className="btn" onClick={toggleModal}>{switchModalButtonText}</button> : <FeedbackRatings feedbackRating={feedbackRating} handleFeedbackRatingChange={handleFeedbackRatingChange} register={register} />}
     </div>
   );
 }
